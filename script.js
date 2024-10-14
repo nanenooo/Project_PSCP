75942fd54fb7178a094513b2d2ce9ddd562327af
Thiarawit <67070085@kmitl.ac.th>
2024-10-14 18:01:51 +0700

    buttom star,stop

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const camera = document.getElementById('video');
         .catch((error) => {
             console.error("ไม่สามารถเข้าถึงกล้องได้: ", error);
         });
+function startDetection() {
+    console.log('Start');
+    // ปิดปุ่ม Start เปิดปุ่ม Stop
+    document.getElementById('start_button').disabled = true;
+    document.getElementById('stop_button').disabled = false;
+}
+function stopDetection() {
+    console.log('Stop');
+    // ปิดปุ่ม Stop เปิดปุ่ม Start
+    document.getElementById('start_button').disabled = false;
+    document.getElementById('stop_button').disabled = true;
+}
