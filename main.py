079a0712a5be76450f8dbe367b84f9bc7fc4129c
nanenooo <137385445+nanenooo@users.noreply.github.com>
2024-11-02 22:41:16 +0700

    landmark lefteye

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import cv2
 import cvzone
 import mediapipe
 from cvzone.FaceMeshModule  import FaceMeshDetector
+
 cap = cv2.VideoCapture(0)
 detector = FaceMeshDetector(maxFaces=1)
+
+#จุดรอบดวงตาซ้าย
+idList = [22, 23, 24, 26, 110, 157, 158, 159 ,160 ,161, 130, 243]
 
 while True:
 
@@ -11,7 +15,26 @@ while True:
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
     success, img = cap.read()
-    img, faces = detector.findFaceMesh(img)
+    img, faces = detector.findFaceMesh(img, draw=False)
+
+    if faces:
+        face = faces[0]
+        for id in idList:
+            #วาดจุดรอบดวงตา
+            cv2.circle(img, face[id], 3, (255, 0, 255), cv2.FILLED)
+
+        #วาดจถดแนวตั้งกับแนวนอน
+        leftUp = face[159]
+        leftDown = face[23]
+        leftLeft = face[130]
+        leftRight = face[243]
+        lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+        lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 2)
+        cv2.line(img, leftLeft, leftRight, (0, 200, 0), 2)
+
+        #แสดงระยะห่างของแกนแนวตั้งกับแนวนอน 
+        print(int((lenghtVer/lenghtHor)*100))
 
       # ตรวจสอบ frame
     if not  success:
