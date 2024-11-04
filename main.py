9bacdca3278990a63edae80eb5cb61e2700ff7ce
nanenooo <137385445+nanenooo@users.noreply.github.com>
2024-11-04 21:57:52 +0700

    eyeArea เปลี่ยนคำนวณ
    เปลี่ยนเป็นการหาพื้นที่สี่เหลี่ยมรูปเว่า ตอนแรกใช้อัตราส่วนของแนวนอนกับแนวตั้ง

--- main.py
+++ main.py
@@ -3,74 +3,77 @@ import cvzone
 from cvzone.PlotModule import LivePlot
 from cvzone.FaceMeshModule  import FaceMeshDetector
 
-cap = cv2.VideoCapture(0)
-detector = FaceMeshDetector(maxFaces=1)
-plotY = LivePlot(640, 360, [20, 50], invert=True)
+def blinkCount():
 
-#จุดรอบดวงตาซ้าย
-idList = [22, 23, 24, 26, 110, 157, 158, 159 ,160 ,161, 130, 243]
-eyeList = []
-blinkCouter = 0
-counter = 0
+    cap = cv2.VideoCapture(0)
+    detector = FaceMeshDetector(maxFaces=1)
+    plotY = LivePlot(640, 360, [20, 50], invert=True)
 
-while True:
+    #จุดรอบดวงตาซ้าย
+    idList = [22, 23, 24, 26, 110, 157, 158, 159 ,160 ,161, 130, 243]
+    eyeList = []
+    blinkCouter = 0
+    counter = 0
 
-    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    while True:
 
-    success, img = cap.read()
-    img, faces = detector.findFaceMesh(img, draw=False)
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    #วาดจุดรอบดวงตา
-    if faces:
-        face = faces[0]
-        for id in idList:
-            cv2.circle(img, face[id], 3, (255, 0, 255), cv2.FILLED)
+        success, img = cap.read()
+        img, faces = detector.findFaceMesh(img, draw=False)
 
-        #วาดจุดแนวตั้งกับแนวนอน
-        leftUp = face[159]
-        leftDown = face[145]
-        leftLeft = face[33]
-        leftRight = face[133]
-        lenghtVer, _ = detector.findDistance(leftUp, leftDown)
-        lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
-        cv2.line(img, leftUp, leftDown, (0, 200, 0), 2)
-        cv2.line(img, leftLeft, leftRight, (0, 200, 0), 2)
+        #วาดจุดรอบดวงตา
+        if faces:
+            face = faces[0]
+            for id in idList:
+                cv2.circle(img, face[id], 3, (255, 0, 255), cv2.FILLED)
 
-        #หาพื้นที่ดวงตา
-        eyeArea = int((lenghtVer/lenghtHor)*100)
-        eyeList.append(eyeArea)
+            #วาดจุดแนวตั้งกับแนวนอน
+            leftUp = face[159]
+            leftDown = face[145]
+            leftLeft = face[33]
+            leftRight = face[133]
+            lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+            lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+            cv2.line(img, leftUp, leftDown, (0, 200, 0), 2)
+            cv2.line(img, leftLeft, leftRight, (0, 200, 0), 2)
 
-        if len(eyeList) > 3:
-            eyeList.pop(0)
+            #หาพื้นที่ดวงตา
+            eyeArea = int((0.05*(lenghtVer*lenghtHor)))
+            eyeList.append(eyeArea)
 
-        eyeAvg = sum(eyeList)/len(eyeList)
-        eyeDiff = abs(eyeAvg - eyeList[-1])
+            if len(eyeList) > 3:
+                eyeList.pop(0)
 
-        if eyeDiff > 4 and counter == 0:
-            blinkCouter += 1
-            counter += 1
-        if counter != 0:
-            counter += 1
-            if counter > 10:
-                counter = 0
+            eyeAvg = sum(eyeList)/len(eyeList)
+            eyeDiff = abs(eyeAvg - eyeList[-1])
 
-        cvzone.putTextRect(img, f"Blink Count: {blinkCouter}", (50,100))
-        imgPlot = plotY.update(eyeAvg)
-        imgStack = cvzone.stackImages([img, imgPlot], 2, 1)
+            if eyeDiff > 4 and counter == 0:
+                blinkCouter += 1
+                counter += 1
+            if counter != 0:
+                counter += 1
+                if counter > 10:
+                    counter = 0
 
-    # ตรวจสอบ frame
-    if not  success:
-        print("Can not connect")
-        break
+            cvzone.putTextRect(img, f"Blink Count: {blinkCouter}", (50,100))
+            imgPlot = plotY.update(eyeAvg)
+            imgStack = cvzone.stackImages([img, imgPlot], 2, 1)
 
-    cv2.imshow("Image", imgStack)
+        # ตรวจสอบ frame
+        if not  success:
+            print("Can not connect")
+            break
 
-    # q for close
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow("Image", imgStack)
 
-    cv2.waitKey(25)
+        # q for close
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.waitKey(25)
+
+    cap.release()
+    cv2.destroyAllWindows()
+blinkCount()
