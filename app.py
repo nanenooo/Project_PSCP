f2b91c8732e36e00a021307c6dd739c3ac01a58c
nanenooo <137385445+nanenooo@users.noreply.github.com>
2024-11-06 16:43:13 +0700

    update app

---
+++ app.py
@@ -0,0 +1,112 @@
+import cv2
+import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
+from flask import Flask, render_template, jsonify, Response
+import threading
+
+app = Flask(__name__)
+
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector(maxFaces=1)
+blinkCounter = 0
+
+def countBlinks():
+    """ ตรวจจับตา และหาการกระพริบตา """
+    global blinkCounter, cap, detector
+
+    leftEyeList, rightEyeList, counter = [], [], 0
+
+    while True:
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        _, img = cap.read()
+        img, faces = detector.findFaceMesh(img, draw=False)
+
+        if faces:
+            face = faces[0]
+            def Counting() :
+            # วาดจุดแนวตั้งกับแนวนอน
+                leftUp, leftDown = face[159], face[145]
+                leftLeft, leftRight = face[33], face[133]
+                rightUp, rightDown = face[243], face[37]
+                rightLeft, rightRight = face[463], face[263]
+                # หาความยาวของเส้น
+                leftLenghtVer, _ = detector.findDistance(leftUp, leftDown)
+                leftLenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+                rightLenghtVer, _ = detector.findDistance(rightUp, rightDown)
+                rightLenghtHor, _ = detector.findDistance(rightLeft, rightRight)
+                cv2.line(img, leftUp, leftDown, (0, 200, 0), 2)
+                cv2.line(img, leftLeft, leftRight, (0, 200, 0), 2)
+                cv2.line(img, rightUp, rightDown, (0, 200, 0), 2)
+                cv2.line(img, rightLeft, rightRight, (0, 200, 0), 2)
+
+                # หาพื้นที่ดวงตา
+                leftEyeArea = int((0.05 * (leftLenghtVer * leftLenghtHor)))
+                rightEyeArea = int((0.05 * (rightLenghtVer * rightLenghtHor)))
+                leftEyeList.append(leftEyeArea)
+                rightEyeList.append(rightEyeArea)
+
+                if len(leftEyeList) > 3 :
+                    leftEyeList.pop(0)
+
+                if len(rightEyeList) > 3 :
+                    rightEyeList.pop(0)
+
+                leftEyeAvg = sum(leftEyeList) / len(leftEyeList)
+                rightEyeAvg = sum(rightEyeList) / len(rightEyeList)
+                leftEyeDiff = abs(leftEyeAvg - leftEyeList[-1])
+                rightEyeDiff = abs(rightEyeAvg - rightEyeList[-1])
+
+                print(rightBlink)
+
+                leftBlink, rightBlink = False, False
+
+                if rightEyeDiff > 4 and counter == 0 :
+                    rightBlink = True
+                if leftEyeDiff > 4 and counter == 0 :
+                    leftBlink = True
+                if leftBlink and rightBlink :
+                    blinkCounter += 1
+                    counter += 1
+                if counter != 0:
+                    counter += 1
+                    if counter > 10:
+                        counter = 0
+
+def generate_frames():
+    """ส่งเฟรมวิดีโอจากกล้อง"""
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        else:
+            # เฟรมเป็น jpg
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+            # ส่งออกเฟรม
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    """ index page """
+    return render_template('index.html')
+
+@app.route('/blink_count', methods=['GET'])
+def get_blinkCount():
+    """ ส่งข้อมูลไปให้ js """
+    global blinkCounter
+    return jsonify(blink_count=blinkCounter)
+
+@app.route('/video_feed')
+def video_feed():
+    """ส่งข้อมูลวิดีโอไปยัง img tag"""
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    blink_thread = threading.Thread(target=countBlinks)
+    blink_thread.daemon = True
+    blink_thread.start()
+    app.run(debug=True, use_reloader=False)
